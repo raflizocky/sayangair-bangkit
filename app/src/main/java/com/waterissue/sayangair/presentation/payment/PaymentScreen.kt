53ea17e3f545089f1a44d47bc935bb1c9d0f2e58
Rafli Zocky Leonard <raflizocky@gmail.com>
2023-12-03 16:16:58 +0700

    add line/divider

--- app/src/main/java/com/waterissue/sayangair/presentation/payment/PaymentScreen.kt
+++ app/src/main/java/com/waterissue/sayangair/presentation/payment/PaymentScreen.kt
@@ -1,8 +1,13 @@
 package com.waterissue.sayangair.presentation.payment
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.waterissue.sayangair.presentation.payment.component.PaymentDetail
 import com.waterissue.sayangair.presentation.payment.component.User
 
@@ -10,10 +15,10 @@ import com.waterissue.sayangair.presentation.payment.component.User
 fun PaymentScreen() {
     Column {
         Banner()
+        Line()
         Payment()
     }
 }
-
 
 @Composable
 fun Banner() {
@@ -24,8 +29,21 @@ fun Banner() {
 
 @Composable
 fun Payment() {
-    PaymentDetail()
+    Column {
+        PaymentDetail()
+    }
 }
+
+@Composable
+fun Line() {
+    Divider(
+        modifier = Modifier
+            .paddingFromBaseline(5.dp),
+        1.dp,
+        Color.Gray
+    )
+}
+
 
 @Composable
 @Preview(showBackground = true)
