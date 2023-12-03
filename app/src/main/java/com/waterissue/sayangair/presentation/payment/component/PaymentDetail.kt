dee38961f0b8f2216e925116245d257f9cce1050
Rafli Zocky Leonard <raflizocky@gmail.com>
2023-12-03 16:18:44 +0700

    add card

--- app/src/main/java/com/waterissue/sayangair/presentation/payment/component/PaymentDetail.kt
+++ app/src/main/java/com/waterissue/sayangair/presentation/payment/component/PaymentDetail.kt
@@ -1,18 +1,31 @@
 package com.waterissue.sayangair.presentation.payment.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,89 +36,139 @@ fun PaymentDetail() {
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
+            .verticalScroll(rememberScrollState())
     ) {
-        Text(
-            text = "Detail Pembayaran",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Text(
-            text = "No. Pelanggan: 123456789",
-            fontSize = 16.sp,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 8.dp)
-        )
-        Text(
-            text = "Tanggal Pembayaran: 01 Desember 2023",
-            fontSize = 16.sp,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Text(
-            text = "Pemakaian: 200 Liter",
-            fontSize = 16.sp,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Text(
-            text = "Total Tagihan: Rp 20.000",
-            fontSize = 16.sp,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Text(
-            text = "Denda: -",
-            fontSize = 16.sp,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Text(
-            text = "Biaya Admin: Rp 2.500",
-            fontSize = 16.sp,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Text(
-            text = "Metode Pembayaran",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 8.dp)
-        )
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.primary),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "Detail Pembayaran",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "No. Pelanggan: 123456789",
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Text(
+                    text = "Tanggal Pembayaran: 01 Desember 2023",
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "Pemakaian: 200 Liter",
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "Total Tagihan: Rp 20.000",
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "Denda: -",
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "Biaya Admin: Rp 2.500",
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+            }
+        }
 
-        MetodePembayaranCard("BCA")
-        MetodePembayaranCard("BRI")
-        MetodePembayaranCard("Mandiri")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "Metode Pembayaran",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+
+                PaymentCard("BCA", Icons.Default.AccountBox)
+                PaymentCard("BRI", Icons.Default.AccountBox)
+                PaymentCard("Mandiri", Icons.Default.AccountBox)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         Button(
             onClick = { /* Handle click on bayar button */ },
             modifier = Modifier
                 .fillMaxWidth()
-                .height(48.dp)
-                .padding(top = 16.dp),
+                .background(MaterialTheme.colorScheme.secondary)
+                .height(48.dp),
         ) {
             Text("Bayar")
         }
     }
 }
 
-
 @Composable
-fun MetodePembayaranCard(namaMetode: String) {
+fun PaymentCard(cardName: String, icon: ImageVector) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
+            .background(MaterialTheme.colorScheme.primary)
             .clickable { /* Handle click on metode pembayaran card */ },
-        shape = RoundedCornerShape(16.dp)
-    ) {
-        Text(
-            text = namaMetode,
-            fontSize = 15.sp,
-            color = Color.Black,
-            modifier = Modifier.padding(16.dp)
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
         )
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = Color.Black,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = cardName,
+                fontSize = 15.sp,
+                color = Color.Black
+            )
+        }
     }
 }
