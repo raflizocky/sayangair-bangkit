ff272448cd52f4748b913c7c892d4acf062d8ca8
Rafli Zocky Leonard <raflizocky@gmail.com>
2023-12-05 21:09:16 +0700

    add other nav

--- app/src/main/java/com/waterissue/sayangair/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/waterissue/sayangair/presentation/home/HomeScreen.kt
@@ -2,11 +2,7 @@ package com.waterissue.sayangair.presentation.home
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -15,29 +11,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.waterissue.sayangair.R
 import com.waterissue.sayangair.presentation.donation.DonationScreen
+import com.waterissue.sayangair.presentation.donation.DonationViewModel
+import com.waterissue.sayangair.presentation.donation.component.ArticleDetail
+import com.waterissue.sayangair.presentation.donation.component.DonationInfoPage
+import com.waterissue.sayangair.presentation.home.component.HomeInfoPage
 import com.waterissue.sayangair.presentation.home.component.NavigationItem
 import com.waterissue.sayangair.presentation.main.component.Screen
 import com.waterissue.sayangair.presentation.payment.PaymentScreen
+import com.waterissue.sayangair.presentation.payment.component.PaymentInfoPage
 import com.waterissue.sayangair.presentation.profile.ProfileScreen
 import com.waterissue.sayangair.presentation.reward.RewardScreen
+import com.waterissue.sayangair.presentation.reward.component.RewardInfoPage
 import com.waterissue.sayangair.presentation.theme.BlueSea
-import com.waterissue.sayangair.presentation.theme.PeachChart
+import com.waterissue.sayangair.presentation.theme.LightSecondary
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
 ) {
+    val viewModel: DonationViewModel = hiltViewModel()
+
     Scaffold(
         bottomBar = {
             BottomBar(navController)
@@ -50,19 +58,43 @@ fun HomeScreen(
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(Screen.Home.route) {
-                HomeActivity()
+                HomeActivity(navController = navController)
             }
             composable(Screen.Reward.route) {
-                RewardScreen()
+                RewardScreen(navController = navController)
             }
             composable(Screen.Payment.route) {
-                PaymentScreen()
+                PaymentScreen(navController = navController)
             }
             composable(Screen.Donation.route) {
-                DonationScreen()
+                DonationScreen(navController = navController, viewModel = viewModel)
+            }
+            composable(
+                "detail/{articleTitle}",
+                arguments = listOf(navArgument("articleTitle") { type = NavType.StringType })
+            ) { backStackEntry ->
+                val articleTitle = backStackEntry.arguments?.getString("articleTitle")
+                val article = viewModel.articles.firstOrNull { it.title == articleTitle }
+                if (article != null) {
+                    ArticleDetail(article = article)
+                } else {
+                    // Handle case where article is not found
+                }
             }
             composable(Screen.Profile.route) {
                 ProfileScreen()
+            }
+            composable("home_info") {
+                HomeInfoPage(navController = navController)
+            }
+            composable("reward_info") {
+                RewardInfoPage(navController = navController)
+            }
+            composable("payment_info") {
+                PaymentInfoPage(navController = navController)
+            }
+            composable("donation_info") {
+                DonationInfoPage(navController = navController)
             }
         }
     }
@@ -82,22 +114,22 @@ private fun BottomBar(
         val navigationItems = listOf(
             NavigationItem(
                 title = stringResource(R.string.menu_home),
-                icon = Icons.Default.Home,
+                icon = ImageVector.vectorResource(id = R.drawable.ic_water),
                 screen = Screen.Home
             ),
             NavigationItem(
                 title = stringResource(R.string.menu_reawrd),
-                icon = Icons.Default.ShoppingCart,
+                icon = ImageVector.vectorResource(id = R.drawable.ic_voucher),
                 screen = Screen.Reward
             ),
             NavigationItem(
                 title = stringResource(R.string.menu_bayar),
-                icon = Icons.Default.Star,
+                icon = ImageVector.vectorResource(id = R.drawable.ic_payment),
                 screen = Screen.Payment
             ),
             NavigationItem(
                 title = stringResource(R.string.menu_donasi),
-                icon = Icons.Default.Favorite,
+                icon = ImageVector.vectorResource(id = R.drawable.ic_donation),
                 screen = Screen.Donation
             ),
             NavigationItem(
@@ -113,7 +145,7 @@ private fun BottomBar(
                     Icon(
                         imageVector = item.icon,
                         contentDescription = item.title,
-                        tint = if (isSelected) PeachChart else Color.Gray
+                        tint = if (isSelected) BlueSea else LightSecondary
                     )
                 },
                 label = { Text(item.title) },
