9b469e059550005c39c5fc16eb1f9c76e946bd06
Rafli Zocky Leonard <raflizocky@gmail.com>
2023-12-05 21:11:10 +0700

    change text and color

--- app/src/main/java/com/waterissue/sayangair/presentation/home/component/CircularProgressBar.kt
+++ app/src/main/java/com/waterissue/sayangair/presentation/home/component/CircularProgressBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -14,15 +15,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.waterissue.sayangair.presentation.theme.BlueSea
+import kotlin.math.absoluteValue
 
 @Composable
 fun CircularProgressBar(
@@ -30,13 +31,13 @@ fun CircularProgressBar(
     number: Int,
     fontSize: TextUnit = 28.sp,
     radius: Dp = 100.dp,
-    baseColor: Color = Color.Gray,
-    progressColor: Color = Color.Blue,
+    color: Color = BlueSea,
     strokeWidth: Dp = 16.dp,
     animDuration: Int = 1000,
     animDelay: Int = 0
 ) {
     var animationPlayed by remember { mutableStateOf(false) }
+
     val curPercentage by animateFloatAsState(
         targetValue = if (animationPlayed) percentage else 0f,
         animationSpec = tween(
@@ -44,39 +45,31 @@ fun CircularProgressBar(
             delayMillis = animDelay
         ), label = ""
     )
+
     LaunchedEffect(key1 = true) {
         animationPlayed = true
     }
-
-    val paint = Paint().asFrameworkPaint()
 
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.size(radius * 2f)
     ) {
         Canvas(modifier = Modifier.size(radius * 2f)) {
+
             drawArc(
-                color = baseColor,
-                startAngle = -90f,
-                sweepAngle = 360f,
+                color = color,
+                -90f,
+                360 * curPercentage.absoluteValue,
                 useCenter = false,
                 style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
             )
-
-            drawArc(
-                color = progressColor,
-                startAngle = -90f,
-                sweepAngle = 360 * curPercentage,
-                useCenter = false,
-                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
-            )
-
-            val text = "${(curPercentage * number).toInt()} liter"
-            paint.textSize = fontSize.toPx()
-            paint.color = Color.Black.toArgb()
-            val xPos = size.width / 2 - paint.measureText(text) / 2
-            val yPos = size.height / 2 - (paint.descent() + paint.ascent()) / 2
-            drawContext.canvas.nativeCanvas.drawText(text, xPos, yPos, paint)
         }
+        Text(
+            text = (curPercentage * number).toInt().toString() + "ℓ",
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
+
